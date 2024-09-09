f60d36e1a8242d3d44b1e9042f6d63b6e5924c2a
Gigi Jishkariani <gigijishkariani3108@gmail.com>
2024-09-09 18:25:35 +0400

    add all figures from classic tetris

--- KUI_Internship_2024/src/tetris/FigureFactory.java
+++ KUI_Internship_2024/src/tetris/FigureFactory.java
@@ -1,9 +1,22 @@
 package tetris;
 
+import java.util.Random;
+
 public class FigureFactory {
 
+	private static final Move[] moves = new Move[] {
+			new Move() { public int[][] move() { return O(); } },
+			new Move() { public int[][] move() { return J(); } },
+			new Move() { public int[][] move() { return T(); } },
+			new Move() { public int[][] move() { return I(); } },
+			new Move() { public int[][] move() { return Z(); } },
+			new Move() { public int[][] move() { return L(); } },
+			new Move() { public int[][] move() { return S(); } },
+	};
+
+
 	public static int[][] createNextFigure() {
-		return O();
+		return moves[new Random().nextInt(moves.length)].move();
 	}
 
 	static int[][] O() {
@@ -23,14 +36,51 @@ public class FigureFactory {
 			{0, 0, 0, 0},
 		};
 	}
-	
-	static int[][] rotatedJ() {
+
+	static int[][] T() {
 		return new int[][] {
-			{0, 0, 0, 0},
-			{0, 2, 0, 0},
-			{0, 2, 2, 2},
-			{0, 0, 0, 0},
+				{0, 0, 0, 0},
+				{0, 0, 0, 0},
+				{0, 0, 3, 0},
+				{0, 3, 3, 3},
 		};
 	}
+
+	static int[][] I() {
+		return new int[][] {
+				{0, 0, 0, 0},
+				{4, 4, 4, 4},
+				{0, 0, 0, 0},
+				{0, 0, 0, 0},
+		};
+	}
+
+	static int[][] Z() {
+		return new int[][] {
+				{0, 0, 0, 0},
+				{5, 5, 0, 0},
+				{0, 5, 5, 0},
+				{0, 0, 0, 0},
+		};
+	}
+
+	static int[][] L() {
+		return new int[][] {
+				{0, 6, 0, 0},
+				{0, 6, 0, 0},
+				{0, 6, 6, 0},
+				{0, 0, 0, 0},
+		};
+	}
+
+	static int[][] S() {
+		return new int[][] {
+				{0, 0, 0, 0},
+				{0, 0, 7, 7},
+				{0, 7, 7, 0},
+				{0, 0, 0, 0},
+		};
+	}
+
 	
 }
